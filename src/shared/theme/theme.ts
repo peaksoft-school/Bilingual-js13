b680d6fb9debc3cc8158524dda8f86fcf2a5683f
hadizha <user@hadizhas-mac.local>
2024-07-06 01:14:50 +0600

    theme was added

---
+++ src/shared/theme/theme.ts
@@ -0,0 +1,36 @@
+import { createTheme, ThemeOptions } from '@mui/material/styles';
+
+export const palette = {
+  primary: {
+    main: '#3A10E5',
+    white: '#FFFFFF',
+    lightBlue: '#D7E1F8',
+    charcoal: '#4C4859',
+  },
+  secondary: {
+    main: '#F61414',
+    green: '#2AB930',
+    lightGray: '#D4D0D0',
+    mediumGray: '#D8D9DB',
+    veryLightGray: '#F7F7F7',
+    purple: '#70719a',
+    blue: '#0F85F1',
+    darkGray: '#757575',
+    inActiveColor: '#C4C4C4',
+    gray: '#9A9A9A',
+  },
+  linear: {
+    linearBlue: 'linear-gradient(270deg, #3A10E5 0%, #6746EF 75.83%)',
+  },
+};
+
+export const typography = {
+  fontFamily: 'Poppins, Gilroy',
+};
+
+const themeOptions: ThemeOptions = {
+  palette: palette,
+  typography: typography,
+};
+
+export const theme = createTheme(themeOptions);
