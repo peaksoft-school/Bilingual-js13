dccbd0884254c63d76bf79e749b3a1e34642ae18
hadizha <user@hadizhas-mac.local>
2024-07-08 12:20:56 +0600

    fixed conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App.tsx'
 import './index.css'
-import { ThemeProvider } from '@mui/material'
-import { theme } from './shared/theme/theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    sdlkndslvFont Gilroy
   </React.StrictMode>,
 )
