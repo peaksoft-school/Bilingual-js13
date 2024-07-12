d2ea5d0d5985b92e8d99e5e7d1478c9ae2cfcfb1
Adilet Zainidinov <adiletzainidinov22@gmail.com>
2024-07-12 09:46:44 +0600

    fixed: I rearranged the folders, put the styles in my folders, added the button cursor: 'pointer';

--- src/widgets/learnMore/components/SecureDesign.tsx
+++ src/widgets/learnMore/components/SecureDesign.tsx
@@ -1,16 +1,10 @@
-import { Box } from '@mui/material'
-import {
-   SecureDesignContainer,
-   SecureDesignSubtitle,
-   SecureDesignTitle,
-   SecureDesignTypographyBox
-} from './StyleLearnMore'
-import DesignLearnMore from '../../../../assets/icons/svgs/DesignLearnMore.svg'
+import {  styled } from '@mui/material'
+import DesignLearnMore from '../../../assets/icons/svgs/DesignLearnMore.svg'
 import { InView } from 'react-intersection-observer'
 import { FC } from 'react'
-import {animationVariants} from './animateLogic'
-
-
+import { animationVariants } from '../model/animateLogic'
+import { motion } from 'framer-motion'
+import { BoxStyle } from './ExpandBox'
 
 const SecureDesign: FC = () => {
    return (
@@ -35,12 +29,51 @@ const SecureDesign: FC = () => {
                   </SecureDesignTypographyBox>
                )}
             </InView>
-            <Box>
+            <BoxStyle>
                <img src={DesignLearnMore} alt="DesignLearnMore" />
-            </Box>
+            </BoxStyle>
          </SecureDesignContainer>
       </>
    )
 }
 
 export default SecureDesign
+
+const SecureDesignContainer = styled('div')(() => {
+   return {
+      width: '100%',
+      maxWidth: '61.6875rem',
+      marginBottom: '8.75rem',
+      display: 'flex',
+      justifyContent: 'space-between',
+   }
+})
+
+const SecureDesignTypographyBox = styled(motion.div)(() => {
+   return {
+      width: '100%',
+      maxWidth: '30.875rem',
+   }
+})
+
+const SecureDesignTitle = styled('h2')(() => {
+   return {
+      fontSize: '1.5rem',
+      fontWeight: '600',
+      color: 'rgba(35, 33, 42, 1)',
+      marginBottom: '1rem',
+      fontFamily: 'Poppins',
+   }
+})
+
+const SecureDesignSubtitle = styled('p')(() => {
+   return {
+      fontSize: '1rem',
+      fontWeight: '400',
+      lineHeight: '1.5rem',
+      color: 'rgba(35, 33, 42, 1)',
+      width: '100%',
+      maxWidth: '30.875rem',
+      fontFamily: 'Poppins',
+   }
+})
