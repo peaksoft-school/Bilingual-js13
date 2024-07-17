94d25f7ec7db31ddb004338cf04827cdd01fe0d9
Aidin <aidinjavascript@gmail.com>
2024-07-17 20:11:44 +0600

    fixed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react'
+import Footer from '../widgets/landing-page/components/Footer'
 const App: FC = () => {
-   return <div></div>
+   return (
+      <div>
+         <Footer />
+      </div>
+   )
 }
 
 export default App
