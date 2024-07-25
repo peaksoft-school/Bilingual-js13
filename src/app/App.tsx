1f7735dea64e1ffa41a42d19567aa2dde67e63be
Rabiya46 <rabiyaaiylchieva@gmail.com>
2024-07-25 12:01:00 +0600

    save

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
+import Prove from '../widgets/prove/components/Prove'
+
 const App = () => {
-   return <div></div>
+   return (
+      <div>
+         <Prove />
+      </div>
+   )
 }
 
 export default App
