c8f54503fdf66e4bc361ce3d7d07533c5f9decbb
Rabiya46 <rabiyaaiylchieva@gmail.com>
2024-07-25 19:19:56 +0600

    merge development

--- src/pages/landing-page/Landing.tsx
+++ src/pages/landing-page/Landing.tsx
@@ -1,10 +1,10 @@
-import Footer from '../../widgets/landing/footer/Footer'
-import Features from '../../widgets/landing/features/Features'
-import LearnMore from '../../widgets/landing/learnMore/components/LearnMore'
-import OurTeam from '../../widgets/landing/our-team/OurTeam'
-import Partners from '../../widgets/landing/partners/components/Partners'
 import Prove from '../../widgets/landing/prove/components/Prove'
+import Footer from '../../widgets/landing/footer/Footer'
+import OurTeam from '../../widgets/landing/our-team/OurTeam'
 import Reviews from '../../widgets/landing/reviews/Reviews'
+import Features from '../../widgets/landing/features/Features'
+import Partners from '../../widgets/landing/partners/components/Partners'
+import LearnMore from '../../widgets/landing/learnMore/components/LearnMore'
 import StatisticsDisplay from '../../widgets/landing/statistics/StatisticsDisplay'
 
 const Landing = () => (
